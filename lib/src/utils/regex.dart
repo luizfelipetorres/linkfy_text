8f1b94d0e8e69125ff4edfd4a7dbf0ab472ef176
luizfelipetorres <fellipe116@gmail.com>
2022-11-10 00:45:58 -0300

    Add characters with accent in usertag regex

--- lib/src/utils/regex.dart
+++ lib/src/utils/regex.dart
@@ -4,7 +4,7 @@ String urlRegExp = r'(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+';
 
 String hashtagRegExp = r'(#+[a-zA-Z0-9(_)]{1,})';
 
-String userTagRegExp = r'(?<![\w@])@([\w@]+(?:[.!][\w@]+)*)';
+String userTagRegExp = r'(?<![\w@])@([\w@À-ú]+(?:[.!][\w@]+)*)';
 String phoneRegExp =
     r'\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*';
 String emailRegExp =
